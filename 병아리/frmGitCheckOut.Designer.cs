266b8eb239bb1e1d010a231c1112812b1454a5c3
techiral137 <125010887+techiral137@users.noreply.github.com>
2024-05-16 23:29:28 +0900

    dddd
    ddd

---
+++ 병아리/frmGitCheckOut.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace IAS.Packages.Git
+{
+	partial class frmGitCheckOut
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+      this.components = new System.ComponentModel.Container();
+      Infragistics.Win.Appearance appearance3 = new Infragistics.Win.Appearance();
+      Infragistics.Win.Appearance appearance4 = new Infragistics.Win.Appearance();
+      this.ultraFormManager1 = new Infragistics.Win.UltraWinForm.UltraFormManager(this.components);
+      this._Form1_UltraFormManager_Dock_Area_Left = new Infragistics.Win.UltraWinForm.UltraFormDockArea();
+      this._Form1_UltraFormManager_Dock_Area_Right = new Infragistics.Win.UltraWinForm.UltraFormDockArea();
+      this._Form1_UltraFormManager_Dock_Area_Top = new Infragistics.Win.UltraWinForm.UltraFormDockArea();
+      this._Form1_UltraFormManager_Dock_Area_Bottom = new Infragistics.Win.UltraWinForm.UltraFormDockArea();
+      this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+      this.btnGitCancel = new Infragistics.Win.Misc.UltraButton();
+      this.btnGitCommit = new Infragistics.Win.Misc.UltraButton();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.ultraGroupBox1 = new Infragistics.Win.Misc.UltraGroupBox();
+      this.chkTagCheckOut = new Infragistics.Win.UltraWinEditors.UltraCheckEditor();
+      this.chkLogicCheckOut = new Infragistics.Win.UltraWinEditors.UltraCheckEditor();
+      this.chkStationCheckout = new Infragistics.Win.UltraWinEditors.UltraCheckEditor();
+      this.chkMMICheckOut = new Infragistics.Win.UltraWinEditors.UltraCheckEditor();
+      this.ultraTree1 = new Infragistics.Win.UltraWinTree.UltraTree();
+      ((System.ComponentModel.ISupportInitialize)(this.ultraFormManager1)).BeginInit();
+      this.tableLayoutPanel2.SuspendLayout();
+      this.tableLayoutPanel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.ultraGroupBox1)).BeginInit();
+      this.ultraGroupBox1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.chkTagCheckOut)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chkLogicCheckOut)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chkStationCheckout)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chkMMICheckOut)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.ultraTree1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // ultraFormManager1
+      // 
+      this.ultraFormManager1.Form = this;
+      // 
+      // _Form1_UltraFormManager_Dock_Area_Left
+      // 
+      this._Form1_UltraFormManager_Dock_Area_Left.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+      this._Form1_UltraFormManager_Dock_Area_Left.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(219)))), ((int)(((byte)(255)))));
+      this._Form1_UltraFormManager_Dock_Area_Left.DockedPosition = Infragistics.Win.UltraWinForm.DockedPosition.Left;
+      this._Form1_UltraFormManager_Dock_Area_Left.ForeColor = System.Drawing.SystemColors.ControlText;
+      this._Form1_UltraFormManager_Dock_Area_Left.FormManager = this.ultraFormManager1;
+      this._Form1_UltraFormManager_Dock_Area_Left.InitialResizeAreaExtent = 8;
+      this._Form1_UltraFormManager_Dock_Area_Left.Location = new System.Drawing.Point(0, 32);
+      this._Form1_UltraFormManager_Dock_Area_Left.Name = "_Form1_UltraFormManager_Dock_Area_Left";
+      this._Form1_UltraFormManager_Dock_Area_Left.Size = new System.Drawing.Size(8, 302);
+      // 
+      // _Form1_UltraFormManager_Dock_Area_Right
+      // 
+      this._Form1_UltraFormManager_Dock_Area_Right.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+      this._Form1_UltraFormManager_Dock_Area_Right.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(219)))), ((int)(((byte)(255)))));
+      this._Form1_UltraFormManager_Dock_Area_Right.DockedPosition = Infragistics.Win.UltraWinForm.DockedPosition.Right;
+      this._Form1_UltraFormManager_Dock_Area_Right.ForeColor = System.Drawing.SystemColors.ControlText;
+      this._Form1_UltraFormManager_Dock_Area_Right.FormManager = this.ultraFormManager1;
+      this._Form1_UltraFormManager_Dock_Area_Right.InitialResizeAreaExtent = 8;
+      this._Form1_UltraFormManager_Dock_Area_Right.Location = new System.Drawing.Point(372, 32);
+      this._Form1_UltraFormManager_Dock_Area_Right.Name = "_Form1_UltraFormManager_Dock_Area_Right";
+      this._Form1_UltraFormManager_Dock_Area_Right.Size = new System.Drawing.Size(8, 302);
+      // 
+      // _Form1_UltraFormManager_Dock_Area_Top
+      // 
+      this._Form1_UltraFormManager_Dock_Area_Top.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+      this._Form1_UltraFormManager_Dock_Area_Top.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(219)))), ((int)(((byte)(255)))));
+      this._Form1_UltraFormManager_Dock_Area_Top.DockedPosition = Infragistics.Win.UltraWinForm.DockedPosition.Top;
+      this._Form1_UltraFormManager_Dock_Area_Top.ForeColor = System.Drawing.SystemColors.ControlText;
+      this._Form1_UltraFormManager_Dock_Area_Top.FormManager = this.ultraFormManager1;
+      this._Form1_UltraFormManager_Dock_Area_Top.Location = new System.Drawing.Point(0, 0);
+      this._Form1_UltraFormManager_Dock_Area_Top.Name = "_Form1_UltraFormManager_Dock_Area_Top";
+      this._Form1_UltraFormManager_Dock_Area_Top.Size = new System.Drawing.Size(380, 32);
+      // 
+      // _Form1_UltraFormManager_Dock_Area_Bottom
+      // 
+      this._Form1_UltraFormManager_Dock_Area_Bottom.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+      this._Form1_UltraFormManager_Dock_Area_Bottom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(219)))), ((int)(((byte)(255)))));
+      this._Form1_UltraFormManager_Dock_Area_Bottom.DockedPosition = Infragistics.Win.UltraWinForm.DockedPosition.Bottom;
+      this._Form1_UltraFormManager_Dock_Area_Bottom.ForeColor = System.Drawing.SystemColors.ControlText;
+      this._Form1_UltraFormManager_Dock_Area_Bottom.FormManager = this.ultraFormManager1;
+      this._Form1_UltraFormManager_Dock_Area_Bottom.InitialResizeAreaExtent = 8;
+      this._Form1_UltraFormManager_Dock_Area_Bottom.Location = new System.Drawing.Point(0, 334);
+      this._Form1_UltraFormManager_Dock_Area_Bottom.Name = "_Form1_UltraFormManager_Dock_Area_Bottom";
+      this._Form1_UltraFormManager_Dock_Area_Bottom.Size = new System.Drawing.Size(380, 8);
+      // 
+      // tableLayoutPanel2
+      // 
+      this.tableLayoutPanel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+      this.tableLayoutPanel2.ColumnCount = 5;
+      this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 3);
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+      this.tableLayoutPanel2.Controls.Add(this.btnGitCancel, 1, 1);
+      this.tableLayoutPanel2.Controls.Add(this.btnGitCommit, 3, 1);
+      this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 253);
+      this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+      this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+      this.tableLayoutPanel2.RowCount = 3;
+      this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+      this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+      this.tableLayoutPanel2.Size = new System.Drawing.Size(364, 49);
+      this.tableLayoutPanel2.TabIndex = 2;
+      // 
+      // btnGitCancel
+      // 
+      appearance3.ForeColor = System.Drawing.Color.White;
+      this.btnGitCancel.Appearance = appearance3;
+      this.btnGitCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.btnGitCancel.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.btnGitCancel.Font = new System.Drawing.Font("Arial", 9F);
+      this.btnGitCancel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+      this.btnGitCancel.Location = new System.Drawing.Point(180, 11);
+      this.btnGitCancel.Margin = new System.Windows.Forms.Padding(0, 3, 0, 3);
+      this.btnGitCancel.Name = "btnGitCancel";
+      this.btnGitCancel.Size = new System.Drawing.Size(80, 27);
+      this.btnGitCancel.TabIndex = 2;
+      this.btnGitCancel.Text = "Cancel";
+      this.btnGitCancel.UseAppStyling = false;
+      this.btnGitCancel.UseFlatMode = Infragistics.Win.DefaultableBoolean.True;
+      this.btnGitCancel.UseOsThemes = Infragistics.Win.DefaultableBoolean.False;
+      // 
+      // btnGitCommit
+      // 
+      appearance4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(102)))), ((int)(((byte)(174)))));
+      appearance4.BorderColor = System.Drawing.Color.Transparent;
+      appearance4.ForeColor = System.Drawing.Color.White;
+      appearance4.TextHAlignAsString = "Center";
+      appearance4.TextVAlignAsString = "Middle";
+      this.btnGitCommit.Appearance = appearance4;
+      this.btnGitCommit.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.btnGitCommit.Font = new System.Drawing.Font("Arial", 9F);
+      this.btnGitCommit.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+      this.btnGitCommit.Location = new System.Drawing.Point(268, 11);
+      this.btnGitCommit.Margin = new System.Windows.Forms.Padding(0, 3, 0, 3);
+      this.btnGitCommit.Name = "btnGitCommit";
+      this.btnGitCommit.Size = new System.Drawing.Size(80, 27);
+      this.btnGitCommit.TabIndex = 1;
+      this.btnGitCommit.Text = "CheckOut";
+      this.btnGitCommit.UseAppStyling = false;
+      this.btnGitCommit.UseFlatMode = Infragistics.Win.DefaultableBoolean.True;
+      this.btnGitCommit.UseOsThemes = Infragistics.Win.DefaultableBoolean.False;
+      this.btnGitCommit.Click += new System.EventHandler(this.btnGitCommit_Click);
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.ColumnCount = 3;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+      this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+      this.tableLayoutPanel1.Controls.Add(this.ultraGroupBox1, 1, 1);
+      this.tableLayoutPanel1.Controls.Add(this.ultraTree1, 1, 0);
+      this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(8, 32);
+      this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 3;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(364, 302);
+      this.tableLayoutPanel1.TabIndex = 0;
+      // 
+      // ultraGroupBox1
+      // 
+      this.ultraGroupBox1.Controls.Add(this.chkTagCheckOut);
+      this.ultraGroupBox1.Controls.Add(this.chkLogicCheckOut);
+      this.ultraGroupBox1.Controls.Add(this.chkStationCheckout);
+      this.ultraGroupBox1.Controls.Add(this.chkMMICheckOut);
+      this.ultraGroupBox1.Location = new System.Drawing.Point(11, 207);
+      this.ultraGroupBox1.Name = "ultraGroupBox1";
+      this.ultraGroupBox1.Size = new System.Drawing.Size(342, 43);
+      this.ultraGroupBox1.TabIndex = 4;
+      this.ultraGroupBox1.Text = "ultraGroupBox1";
+      // 
+      // chkTagCheckOut
+      // 
+      this.chkTagCheckOut.Location = new System.Drawing.Point(187, 17);
+      this.chkTagCheckOut.Name = "chkTagCheckOut";
+      this.chkTagCheckOut.Size = new System.Drawing.Size(120, 20);
+      this.chkTagCheckOut.TabIndex = 3;
+      this.chkTagCheckOut.Text = "Tag";
+      // 
+      // chkLogicCheckOut
+      // 
+      this.chkLogicCheckOut.Location = new System.Drawing.Point(129, 17);
+      this.chkLogicCheckOut.Name = "chkLogicCheckOut";
+      this.chkLogicCheckOut.Size = new System.Drawing.Size(120, 20);
+      this.chkLogicCheckOut.TabIndex = 2;
+      this.chkLogicCheckOut.Text = "Logic";
+      // 
+      // chkStationCheckout
+      // 
+      this.chkStationCheckout.Location = new System.Drawing.Point(61, 17);
+      this.chkStationCheckout.Name = "chkStationCheckout";
+      this.chkStationCheckout.Size = new System.Drawing.Size(120, 20);
+      this.chkStationCheckout.TabIndex = 1;
+      this.chkStationCheckout.Text = "Station";
+      // 
+      // chkMMICheckOut
+      // 
+      this.chkMMICheckOut.Location = new System.Drawing.Point(6, 17);
+      this.chkMMICheckOut.Name = "chkMMICheckOut";
+      this.chkMMICheckOut.Size = new System.Drawing.Size(120, 20);
+      this.chkMMICheckOut.TabIndex = 0;
+      this.chkMMICheckOut.Text = "MMI";
+      // 
+      // ultraTree1
+      // 
+      this.ultraTree1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.ultraTree1.Location = new System.Drawing.Point(11, 3);
+      this.ultraTree1.Name = "ultraTree1";
+      this.ultraTree1.Size = new System.Drawing.Size(342, 198);
+      this.ultraTree1.TabIndex = 5;
+      // 
+      // frmGitCheckOut
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(380, 342);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.Controls.Add(this._Form1_UltraFormManager_Dock_Area_Left);
+      this.Controls.Add(this._Form1_UltraFormManager_Dock_Area_Right);
+      this.Controls.Add(this._Form1_UltraFormManager_Dock_Area_Top);
+      this.Controls.Add(this._Form1_UltraFormManager_Dock_Area_Bottom);
+      this.Name = "frmGitCheckOut";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.frmGitCheckOut_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.ultraFormManager1)).EndInit();
+      this.tableLayoutPanel2.ResumeLayout(false);
+      this.tableLayoutPanel1.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.ultraGroupBox1)).EndInit();
+      this.ultraGroupBox1.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.chkTagCheckOut)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chkLogicCheckOut)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chkStationCheckout)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chkMMICheckOut)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.ultraTree1)).EndInit();
+      this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private Infragistics.Win.UltraWinForm.UltraFormManager ultraFormManager1;
+		private Infragistics.Win.UltraWinForm.UltraFormDockArea _Form1_UltraFormManager_Dock_Area_Left;
+		private Infragistics.Win.UltraWinForm.UltraFormDockArea _Form1_UltraFormManager_Dock_Area_Right;
+		private Infragistics.Win.UltraWinForm.UltraFormDockArea _Form1_UltraFormManager_Dock_Area_Top;
+		private Infragistics.Win.UltraWinForm.UltraFormDockArea _Form1_UltraFormManager_Dock_Area_Bottom;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private Infragistics.Win.Misc.UltraButton btnGitCancel;
+		private Infragistics.Win.Misc.UltraButton btnGitCommit;
+    private Infragistics.Win.Misc.UltraGroupBox ultraGroupBox1;
+    private Infragistics.Win.UltraWinEditors.UltraCheckEditor chkTagCheckOut;
+    private Infragistics.Win.UltraWinEditors.UltraCheckEditor chkLogicCheckOut;
+    private Infragistics.Win.UltraWinEditors.UltraCheckEditor chkStationCheckout;
+    private Infragistics.Win.UltraWinEditors.UltraCheckEditor chkMMICheckOut;
+    private Infragistics.Win.UltraWinTree.UltraTree ultraTree1;
+  }
+}
---
+++ 병아리/frmGitCheckOut.cs
@@ -0,0 +1,59 @@
+﻿using Infragistics.Portable.Gauges.VisualData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using IAS.Packages.Git.Command;
+using System.Windows.Forms;
+using LibGit2Sharp;
+using System.IO;
+using Infragistics.Win.UltraWinTree;
+
+namespace IAS.Packages.Git
+{
+	public partial class frmGitCheckOut : Form
+	{
+		public string shaValue { get; set; }
+		public string commitMessage { get; set; }
+		public string commitDate { get; set; }
+		public object CommitFiles { get; set; }	
+
+		private RevisionControl revisionControl;
+
+		public frmGitCheckOut()
+		{
+			InitializeComponent();
+		}
+
+		private void btnGitCommit_Click(object sender, EventArgs e)
+		{
+		}
+		private void GitRollBack()
+		{
+		}
+		private void CheckoutSpecificFile()
+		{
+    }
+
+    private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+    {
+
+    }
+
+    private void frmGitCheckOut_Load(object sender, EventArgs e)
+    {
+      List<string> files = new List<string>((IEnumerable<string>)CommitFiles);
+
+      // 파일 목록을 UltraTree에 추가합니다.
+      foreach (var file in files)
+      {
+        ultraTree1.Nodes.Add(file);
+      }
+    }
+  }
+}
